f3e9d3e5fb3b5af2bdeeac75ec0a36896e9f62ec
ryzhov <9920529@gmail.com>
2024-05-06 10:35:30 +0300

    feat(SG-205): mutation event, eventbus in base controller

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,8 +1,9 @@
 import { EventSingUpDataInterface } from "./registered-events/event-sign-up";
 import { EventTestDataInterface } from "./registered-events/event-test";
+import { EventMutationDataInterface } from "./registered-events/event-mutation";
 
 export { EventInterface } from "./event.interface";
 export { EventSingUpDataInterface, EventSignUp } from "./registered-events/event-sign-up";
 export { EventTestDataInterface, EventTest } from "./registered-events/event-test";
 
-export type RegisteredEvent = EventSingUpDataInterface | EventTestDataInterface;
+export type RegisteredEvent = EventSingUpDataInterface | EventTestDataInterface | EventMutationDataInterface;
--- src/http/base.controller.ts
+++ src/http/base.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import { LoggerInterface } from "../logger/interfaces/logger.interface";
 import { container } from "tsyringe";
+import { EventbusService } from "../eventbus";
 
 export abstract class BaseController {
 	protected logger: LoggerInterface;
 	protected request: Request;
 	protected response: Response;
+	protected eventBus: EventbusService;
 
 	constructor() {
 		this.logger = container.resolve<LoggerInterface>("Logger");
 		this.logger.module = this.constructor.name || "Controller";
+		this.eventBus = container.resolve<EventbusService>("EventbusService");
 	}
 
 	public init(request: Request, response: Response) {
