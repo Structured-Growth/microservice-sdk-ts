7bcf80245ec64e718e68c0890eb47f68ecb724fa
ryzhov <9920529@gmail.com>
2024-06-18 12:40:26 +0300

    fix(SG-216): policies service draft

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,6 @@
 export { AuthenticatedAccountInterface } from "./interfaces/authenticated-account.interface";
 export { GuestPrincipalInterface } from "./interfaces/guest-principal.interface";
 export { AuthService } from "./auth.service";
+export { PolicyService } from "./policy.service";
 export { AuthTestService } from "./auth-test.service";
 export { AuthServiceInterface } from "./interfaces/auth-service.interface";
