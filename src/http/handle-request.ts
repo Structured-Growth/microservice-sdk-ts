2faf68c03a33783ff2ea7a7ad3d62cb2945f8b7f
anton-devline <anton.z@devline.pro>
2025-03-20 09:41:31 +0000

    feat(SG-575): i18n object in async threads

--- src/http/handle-request.ts
+++ src/http/handle-request.ts
@@ -17,16 +17,9 @@ container.register("i18n", {
 	},
 });
 
-const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE || "en-US";
-const supportedLngs = process.env.DEFAULT_AVAILABLE_LANGUAGES?.split(",") || ["en"];
-const TRANSLATE_API_URL = process.env.TRANSLATE_API_URL;
-const TRANSLATE_API_CLIENT_ID = process.env.TRANSLATE_API_CLIENT_ID;
-
-console.log("PATH: ", path.join(process.cwd(), "src", "i18n", "locales"));
-
 i18n.configure({
-	locales: supportedLngs,
-	defaultLocale: DEFAULT_LANGUAGE,
+	locales: process.env.DEFAULT_AVAILABLE_LANGUAGES?.split(",") || ["en"],
+	defaultLocale: process.env.DEFAULT_LANGUAGE || "en-US",
 	updateFiles: false,
 	queryParameter: "lang",
 	objectNotation: true,
@@ -38,7 +31,7 @@ i18n.configure({
 
 async function loadTranslations(lang: string) {
 	try {
-		const url = `${TRANSLATE_API_URL}/v1/translation-set/${TRANSLATE_API_CLIENT_ID}/${lang}`;
+		const url = `${process.env.TRANSLATE_API_URL}/v1/translation-set/${process.env.TRANSLATE_API_CLIENT_ID}/${lang}`;
 		const response = await signedInternalFetch(url, {
 			method: "get",
 			headers: {
@@ -66,10 +59,19 @@ export function handleRequest(
 	logger.module = "Http";
 
 	return async function (req: Request, res: Response) {
-		const i18nInstance = Object.create(i18n);
-		const acceptLanguage = req.headers["accept-language"]?.split(",")[0].split(";")[0];
-		const lang = supportedLngs.includes(acceptLanguage) ? acceptLanguage : DEFAULT_LANGUAGE;
+		const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE || "en-US";
+		const supportedLngs = process.env.DEFAULT_AVAILABLE_LANGUAGES?.split(",") || ["en"];
+		const TRANSLATE_API_URL = process.env.TRANSLATE_API_URL;
+		const TRANSLATE_API_CLIENT_ID = process.env.TRANSLATE_API_CLIENT_ID;
 
+		const i18nInstance = Object.create(i18n);
+		const acceptLanguageHeader = req.headers["Accept-Language"];
+		const acceptLanguage = Array.isArray(acceptLanguageHeader)
+			? acceptLanguageHeader[0]?.split(",")[0].split(";")[0]
+			: acceptLanguageHeader?.split(",")[0].split(";")[0];
+
+		const lang = supportedLngs.includes(acceptLanguage) ? acceptLanguage : DEFAULT_LANGUAGE;
+		console.log("Lang: ", lang);
 		i18nInstance.setLocale(lang);
 
 		if (lang !== DEFAULT_LANGUAGE && TRANSLATE_API_URL && TRANSLATE_API_CLIENT_ID) {
@@ -77,6 +79,7 @@ export function handleRequest(
 
 			if (Object.keys(translations).length > 0) {
 				i18nInstance.__ = (key: string) => {
+					console.log("key: ", key);
 					const keys = key.split(".");
 					let result = translations;
 
