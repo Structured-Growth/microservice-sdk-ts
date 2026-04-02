4ccda93536ebd0acd13786f309ceed1d05db6384
anton-devline <anton.z@devline.pro>
2026-04-02 13:02:55 +0100

    feat(SG-694): Updating principal types in the SDK microservice

--- src/auth/interfaces/guest-principal.interface.ts
+++ src/auth/interfaces/guest-principal.interface.ts
@@ -1,6 +1,15 @@
+import { RegionEnum } from "../../interfaces";
 import { PrincipalTypeEnum } from "./principal-type.enum";
 
 export interface GuestPrincipalInterface {
+	id: number | string;
+	orgId: number;
+	parentOrgIds: number[];
+	region: RegionEnum;
 	type: PrincipalTypeEnum.GUEST;
 	arn: string;
+	firstName?: string;
+	lastName?: string;
+	email?: string;
+	image?: string | null;
 }
