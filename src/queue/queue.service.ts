c9c41acfb938a2c32bbc4bd0e3bc65e9d741e0e1
Artem Ryzhov <aryzhov@allezhealth.com>
2026-06-25 10:27:28 +0300

    fix(SG-711): resources in queues

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -6,8 +6,8 @@ import { QueueProviderInterface } from "./interfaces/queue-provider.interface";
 export class QueueService implements QueueInterface {
 	constructor(@inject("QueueProvider") private queueProvider: QueueProviderInterface) {}
 
-	public async publish(queueName: string, subject: string, message: object): Promise<boolean> {
-		return this.queueProvider.publish(queueName, subject, message);
+	public async publish(queueName: string, subject: string, message: object, resources?: string[]): Promise<boolean> {
+		return this.queueProvider.publish(queueName, subject, message, resources);
 	}
 
 	public subscribe(queueName: string, handler) {
